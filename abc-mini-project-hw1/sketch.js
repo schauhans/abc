3ddcda35289f2a97d3b363b6be9ed49e215c93f6
schauhans <sc10486@nyu.edu>
2026-01-22 15:50:49 -0500

    now can show ripples on finger drag

--- abc-mini-project-hw1/sketch.js
+++ abc-mini-project-hw1/sketch.js
@@ -1,12 +1,27 @@
 let ripples = []; //global var to keep track of ripples still in motion
+lastSpawn = 0; //time when last ripple was spawned (start at 0ms)
 
 function setup(){
     let canvas = createCanvas(windowWidth, windowHeight);
     canvas.parent("root");
-    background(173, 216, 230); //pink background
+    background(173, 216, 230); 
+    noFill();
+}
+
+function addRipples(){
+    for(let i = 0; i < touches.length; i++){
+        let x = touches[i].x;
+        let y = touches[i].y;
+        ripples.push({
+            x, y, size: 10, alpha: 200
+        });
+    } 
 }
 
 function draw(){
+
+    background(173, 216, 230); //prevent buildup
+
     for(let r of ripples){
         stroke(0, r.alpha);
         circle(r.x, r.y, r.size);
@@ -15,14 +30,19 @@ function draw(){
     }
 
     ripples.filter(r => r.alpha > 0); //remove ripples that are fully faded out
+
 }
 
 function touchStarted(){
-    for(let i = 0; i < touches.length; i++){
-        let x = touches[i].x;
-        let y = touches[i].y;
-        ripples.push({
-            x, y, size: 10, alpha: 200
-        });
-    } 
+    addRipples();
+    return false; //prevent drag (accoding to Chatgpt)
+}
+
+function touchMoved(){
+    const now = millis(); //updates every couple ms
+    if (now - lastSpawn > 50) {
+      addRipples();
+      lastSpawn = now; //update last spawn time
+    }
+    return false;
 }
